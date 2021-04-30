743bfef1a4f8d64307ed9311d89b49f0e1a144af
Felix Reyero <felixreyero@gmail.com>
2021-04-30 20:47:29 -0300

    Fixed test

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,8 @@ import json
 VALID_REGISTER = json.dumps(
     {'first_name': 'Steven', 'last_name': 'Seagal', 'email': 'steven@seagal.com', 'genre': 1,
      'native_language': 1, 'practice_language': 1, 'actual_level': 1, 'birth_date': '10/04/1952',
-     'topics_descriptions': 'Kung Fu and Killing Bad Guys'})
+     'topics_descriptions': 'Kung Fu and Killing Bad Guys',
+     'profile_picture': "https://images.mubicdn.net/images/cast_member/28365/cache-3386-1478101707/image-w856.jpg?size=400x"})
 
 
 INVALID_MINOR_REGISTER = json.dumps(
