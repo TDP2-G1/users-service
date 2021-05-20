74f8b707e659eb7017f34af1708f202a18acfaaf
Felix Reyero <felixreyero@gmail.com>
2021-05-20 18:50:20 -0300

    Agregados TESTS para Feebacks, y sumados otros para Genre, Language y Level

--- tests/test_level_creation.py
+++ tests/test_level_creation.py
@@ -29,3 +29,14 @@ class FlaskTest(unittest.TestCase):
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(data['Error'], "level already exists in database.")
         self.assertEqual(status_code, 403)
+
+
+    def test_valid_level_creation_get(self):
+        tester = create_app().test_client(self)
+        response = level_creation(tester, 'Basic')
+        response = tester.get("/level/", content_type='application/json')
+        status_code = response.status_code
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data[0]['id_level'], 1)
+        self.assertEqual(data[0]['level_description'], "Basic")
+        self.assertEqual(status_code, 200)
