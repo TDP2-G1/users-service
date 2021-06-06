af88452e3219879c670ffb126975917145193c8b
Felix Reyero <felixreyero@gmail.com>
2021-06-05 21:05:12 -0300

    added tests for report

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,19 +12,16 @@ ANOTHER_VALID_REGISTER = json.dumps(
      'topics_descriptions': 'Kung Fu Tournaments',
      'profile_picture': "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2020/10/jean-claude-van-damme-2104337.jpg?itok=Ijy1-cuj"})
 
-
 INVALID_MINOR_REGISTER = json.dumps(
     {'fb_user_id': "u23y48218", 'first_name': 'Little', 'last_name': 'Kid', 'email': 'little@kid.com', 'genre': 1,
      'native_language': 1, 'practice_language': 1, 'actual_level': 1, 'birth_date': '10/04/2006',
      'topics_descriptions': 'Child topics'})
-
 
 INVALID_REGISTER_DATE_FORMAT = json.dumps(
     {'fb_user_id': "u23y48298", 'first_name': 'Steven', 'last_name': 'Seagal', 'email': 'steven@seagal.com', 'genre': 1,
      'native_language': 1, 'practice_language': 1, 'actual_level': 1, 'birth_date': '10/04/52',
      'topics_descriptions': 'Kung Fu and Killing Bad Guys',
      'profile_picture': "https://images.mubicdn.net/images/cast_member/28365/cache-3386-1478101707/image-w856.jpg?size=400x"})
-
 
 VALID_FEEDBACK = json.dumps(
     {
@@ -33,9 +30,15 @@ VALID_FEEDBACK = json.dumps(
         "id_user_receiver": 1
     })
 
-
 VALID_FOLLOWER = json.dumps(
     {
         "id_user_1": 2,
         "id_user_2": 1
     })
+
+VALID_REPORT = json.dumps(
+    {
+        "id_report_type": 1,
+        "id_user_reported": 1,
+        "id_user_reported_by": 2
+    });
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,7 +1,6 @@
 import json
 
-from tests import VALID_REGISTER, INVALID_MINOR_REGISTER, INVALID_REGISTER_DATE_FORMAT, ANOTHER_VALID_REGISTER, \
-    VALID_FEEDBACK
+from tests import VALID_REGISTER, ANOTHER_VALID_REGISTER, VALID_REPORT
 from tests.test_genre_creation import genre_creation
 from tests.test_language_creation import language_creation
 from tests.test_level_creation import level_creation
@@ -11,43 +10,56 @@ import unittest
 
 class FlaskTest(unittest.TestCase):
 
-    def test_valid_feedback(self):
+    def test_valid_report(self):
         tester = create_app().test_client(self)
         genre_creation(tester, 'Male')
         language_creation(tester, 'English')
         level_creation(tester, 'Advanced')
+        tester.post("/report_type/", data=json.dumps({'report_type_description': 'Algo malo'}),
+                    content_type='application'
+                                 '/json')
         response = tester.post("/user/", data=VALID_REGISTER, content_type='application/json')
         response = tester.post("/user/", data=ANOTHER_VALID_REGISTER, content_type='application/json')
-        response = tester.post("/feedback/", data=VALID_FEEDBACK, content_type='application/json')
+        response = tester.post("/report/", data=VALID_REPORT, content_type='application/json')
         status_code = response.status_code
         data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(data['id'], 1)
+        self.assertEqual(data['Status'], 'Report Created')
+        self.assertEqual(status_code, 200)
+        response = tester.get("/user/u23y48298", content_type='application/json')
+        status_code = response.status_code
+        data = json.loads(response.get_data(as_text=True))
+        print(data)
+        self.assertEqual(data['reported_by'][0], 2)
         self.assertEqual(status_code, 200)
 
-
-    def test_invalid_feedback_not_existent_users(self):
+    def test_invalid_report_not_existent_users(self):
         tester = create_app().test_client(self)
         genre_creation(tester, 'Male')
         language_creation(tester, 'English')
         level_creation(tester, 'Advanced')
-        response = tester.post("/feedback/", data=VALID_FEEDBACK, content_type='application/json')
+        tester.post("/report_type/", data=json.dumps({'report_type_description': 'Algo malo'}),
+                    content_type='application'
+                                 '/json')
+        response = tester.post("/report/", data=VALID_REPORT, content_type='application/json')
         status_code = response.status_code
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(data['Error'], 'User ID does not exist.')
         self.assertEqual(status_code, 404)
 
-
-    def test_valid_feedback_get(self):
+    def test_valid_report_get(self):
         tester = create_app().test_client(self)
         genre_creation(tester, 'Male')
         language_creation(tester, 'English')
         level_creation(tester, 'Advanced')
+        tester.post("/report_type/", data=json.dumps({'report_type_description': 'Algo malo'}),
+                    content_type='application'
+                                 '/json')
         response = tester.post("/user/", data=VALID_REGISTER, content_type='application/json')
         response = tester.post("/user/", data=ANOTHER_VALID_REGISTER, content_type='application/json')
-        response = tester.post("/feedback/", data=VALID_FEEDBACK, content_type='application/json')
-        response = tester.get("/feedback/1", content_type='application/json')
+        response = tester.post("/report/", data=VALID_REPORT, content_type='application/json')
+        response = tester.get("/report/1", content_type='application/json')
         status_code = response.status_code
         data = json.loads(response.get_data(as_text=True))
         print(data)
-        self.assertEqual(data[0]['id_user_giver'], 2)
+        self.assertEqual(data[0]['id_report'], 1)
         self.assertEqual(status_code, 200)
